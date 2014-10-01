726407905038aed49c72766a31b09ffaa85cd9cb
Shin Ilyong <yoiziy@gmail.com>
2014-10-01 22:58:11 +0900

    [API 기초 작업 #14] Test API - /getPhoneInfoOfMayor

--- seoul_serenity/api/views.py
+++ seoul_serenity/api/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
+from flask import jsonify
 # from flask.ext.login import login_required
 
 blueprint = Blueprint("api", __name__, url_prefix='/api',
@@ -10,3 +11,13 @@ blueprint = Blueprint("api", __name__, url_prefix='/api',
 #@login_required
 def api_introduce():
     return render_template("api/introduce.html")
+
+@blueprint.route("/getPhoneInfoOfMayor", methods=["GET", "POST"]) #@app.route('/users/<userid>', methods = ['GET'])
+def getPhoneModelOfMayor(): #def api_users(userid):
+	userid = '1'
+	users = {'Manufacture':'Apple', 'PhoneNumber':'010-XXXX-XXXX', 'Model':'iPhone5S'}
+	#if userid in users:
+	#return jsonify({userid:users[userid]})
+	return jsonify(users)
+	#else:
+	#	return not_found()
