62a390f7aed46b4470fe623293a02e421e00fc40
jinmin <mwjin0312@gmail.com>
2014-11-02 18:58:01 +0900

    dateField 변경 .

--- seoul_serenity/project/forms.py
+++ seoul_serenity/project/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import Form
 
 from wtforms import TextField, PasswordField
-from wtforms.ext.dateutil.fields import DateTimeField
+from wtforms.fields.html5 import DateField
+
 
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
@@ -11,8 +12,8 @@ from .models import Project
 class RegisterProjectForm(Form):
 	name = TextField('Project', validators=[DataRequired(), Length(min=3)])
 
-	start_date = DateTimeField('Start Date', validators=[DataRequired()])
-	end_date = DateTimeField('End Date', validators=[DataRequired()])
+	start_date = DateField('Start Date', validators=[DataRequired()])
+	end_date = DateField('End Date', validators=[DataRequired()])
 	description = TextField('Description', validators=[Length(min=0)])
 
 	## TODO : session id ( users.id )  
