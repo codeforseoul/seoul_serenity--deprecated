9bd5ba713ad054cc9ef405da179e3b6dfec0a122
Ryan y. Seo <kueilove@gmail.com>
2014-12-02 14:05:05 +0900

    Change routing rule for pagination

--- seoul_serenity/admin/views.py
+++ seoul_serenity/admin/views.py
@@ -10,6 +10,7 @@ from seoul_serenity.extensions import login_manager
 from seoul_serenity.project.models import Project
 from seoul_serenity.public.forms import LoginForm
 from seoul_serenity.user.forms import RegisterForm
+from seoul_serenity.user.models import User
 from seoul_serenity.utils import flash_errors
 from seoul_serenity.database import db
 
@@ -23,27 +24,53 @@ def index():
   return render_template("admin/index.html")
 
 # TODO 서영태 : Pagination 적용 (MUST)
+#  TODO : Crate button to remove search result
 @blueprint.route("/projects/")
-@blueprint.route("/projects/index/")
-@blueprint.route("/projects/index/<int:page_num>")
-@blueprint.route("/projects/index/<query>")
-@blueprint.route("/projects/index/<query>/<int:page_num>")
 # @login_required
-def projects(page_num=1, query=None):
-  # PER_PAGE config 위치 재설정 필요
-  PER_PAGE = 2
+def projects():
+  # TODO : add Try Catch
+  # TODO : add button(or dropbox) for items_per_page
+  page_num = request.args.get('page_num',default=1, type=int)
+  query = request.args.get('search_text')
+  items_per_page = request.args.get('per_page',default=10, type=int)
 
-  if request.args.get('project'):
-    param = request.args.get('project')
-    return redirect(url_for('admin.projects', query=param))
+  # yoseo : temporary removed
+  # if request.args.get('project'):
+  #   param = request.args.get('project')
+  #   return redirect(url_for('admin.projects', query=param))
+  # else:
+  if query == None:
+    pagination = Project.query.paginate(page_num, items_per_page, False)
   else:
-    if query == None:
-      pagination = Project.query.paginate(page_num, PER_PAGE, False)
-    else:
-      pagination = Project.query.filter(Project.name.like(unicode('%' + query + '%'))).paginate(page_num, PER_PAGE, False)
+    pagination = Project.query.filter(Project.name.like(unicode('%' + query + '%'))).paginate(page_num, items_per_page, False)
   return render_template("admin/projects.html", pagination=pagination, query=query)
 
 
+# TODO 서영태 : Pagination 적용 (MUST)
+#  TODO : Crate button to remove search result
+@blueprint.route("/users/")
+# @login_required
+def users():
+  # TODO : add Try Catch
+  # TODO : add button(or dropbox) for items_per_page
+  page_num = request.args.get('page_num',default=1, type=int)
+  query = request.args.get('search_text')
+  items_per_page = request.args.get('per_page',default=10, type=int)
+
+  # yoseo : temporary removed
+  # if request.args.get('project'):
+  #   param = request.args.get('project')
+  #   return redirect(url_for('admin.projects', query=param))
+  # else:
+  if query == None:
+    pagination = User.query.paginate(page_num, items_per_page, False)
+  else:
+    pagination = User.query.filter(User.name.like(unicode('%' + query + '%'))).paginate(page_num, items_per_page, False)
+  return render_template("admin/users.html", pagination=pagination, query=query)
+
+
+
+# Test page for layout
 @blueprint.route("/view_test")
 def view_test():
   return render_template("admin/test.html")
